b689b72837f8625bea4d62be0bd5357b79ca1e9d
vbarnych <54506158+vbarnych@users.noreply.github.com>
2019-12-27 09:05:54 +0200

    add index.html script.js

---
+++ src/script.js
@@ -0,0 +1,5 @@
+let r = +prompt("Enter circle's radius : ");
+alert(`Square = ${Math.PI*r*r}`);
+let a = +prompt("Enter 1st catheter : ");
+let b = +prompt("Enter 2nd catheter : ");
+alert(`Hypotenuse = ${Math.sqrt(a*a+b*b)}`);
